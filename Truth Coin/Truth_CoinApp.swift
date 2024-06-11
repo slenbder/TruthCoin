3be55f75aa6af5f870620e23bd975f1f62d37dc5
slenbder <152342422+slenbder@users.noreply.github.com>
2024-06-11 13:55:47 +0300

    Initial Commit

---
+++ Truth Coin/Truth_CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Truth_CoinApp.swift
+//  Truth Coin
+//
+//  Created by Кирилл Марьясов on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Truth_CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
