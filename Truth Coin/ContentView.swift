29309b325dd3bb28477378ea8901ac575de0ef87
slenbder <152342422+slenbder@users.noreply.github.com>
2024-06-13 16:45:23 +0300

    Ебемся с текстурами

--- Truth Coin/ContentView.swift
+++ Truth Coin/ContentView.swift
@@ -24,33 +24,23 @@ struct CoinView: UIViewRepresentable {
         sceneView.allowsCameraControl = false
         sceneView.autoenablesDefaultLighting = true
         sceneView.backgroundColor = UIColor.clear
-        
-        if let coinScene = SCNScene(named: "Coin.scn") {
-            if let coinNode = coinScene.rootNode.childNodes.first {
-                let parentNode = SCNNode()
-                parentNode.addChildNode(coinNode)
-                
-                // Масштабируем родительский узел
-                parentNode.scale = SCNVector3(0.01, 0.01, 0.01) // Уменьшаем размер монеты
-                
-                // Устанавливаем позицию родительского узла
-                parentNode.position = SCNVector3(0, 0, 0) // Центруем монетку
-                
-                sceneView.scene?.rootNode.addChildNode(parentNode)
-                
-                let rotationAnimation = CABasicAnimation(keyPath: "rotation") // Вертим монетку на хуе
-                rotationAnimation.fromValue = SCNVector4(0, 1, 0, 0)
-                rotationAnimation.toValue = SCNVector4(0, 1, 0, CGFloat.pi * 2)
-                
-                parentNode.addAnimation(rotationAnimation, forKey: "rotation")
+
+        if let sceneURL = Bundle.main.url(forResource: "moneta", withExtension: "obj"),
+           let sceneSource = SCNSceneSource(url: sceneURL, options: nil) {
+            do {
+                let scene = try sceneSource.scene(options: nil)
+                let node = scene.rootNode.childNodes.first
+                node?.scale = SCNVector3(0.01, 0.01, 0.01) // Уменьшаем размер монеты
+                sceneView.scene?.rootNode.addChildNode(node!)
+            } catch {
+                print("Ошибка загрузки сцены: \(error)")
             }
         }
         
         return sceneView
     }
-    
-    func updateUIView(_ uiView: SCNView, context: Context) {
-    }
+
+    func updateUIView(_ uiView: SCNView, context: Context) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
